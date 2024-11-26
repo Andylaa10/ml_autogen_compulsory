5e4b04da03310611ca45dedf18a22aeb03176ef4
Andy lam <91528350+Andylaa10@users.noreply.github.com>
2024-11-26 16:13:19 +0100

    Updated the agent to verify the code

--- generate_code_agent/agent/generate_code_agent.py
+++ generate_code_agent/agent/generate_code_agent.py
@@ -5,7 +5,6 @@ from config import LLM_CONFIG
 system_prompt = """
 You are now an AI agent capable of writing PYTHON code. 
 When you are done writing python function/s and example of how to use the function/s, 
-THEN REPLY TERMINATE THE PROCESS AND PRINT TERMINATE IN THE CONSOLE!
 """
 
 # Create the agent responsible for generating code
@@ -21,8 +20,11 @@ def create_generate_code_agent() -> ConversableAgent:
 def create_user_proxy() -> ConversableAgent:
     user_proxy = ConversableAgent(
         name="User",
-        llm_config=False,
+        max_consecutive_auto_reply=2,
+        llm_config=LLM_CONFIG,
         is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
+        code_execution_config={"work_dir": "web", "use_docker": True},
         human_input_mode="NEVER",
+        system_message="REPLY TERMINATE THE PROCESS AND PRINT TERMINATE IN THE CONSOLE! IF THE TASK HAS BEEN SOLVED AT FULL SATISFACTION."
     )
     return user_proxy
